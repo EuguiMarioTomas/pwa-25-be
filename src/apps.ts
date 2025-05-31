1c26ee88e2db5ea2a2711c8ed00a0b47bd6b9b98
Eugui Tomas <tomasmarioeugui@gmail.com>
2025-05-31 00:12:59 -0300

    uso de de postRoutes

--- src/apps.ts
+++ src/apps.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import userRoutes from './routes/user_routes';
-//import postRoutes from './routes/post_routes';
+import postRoutes from './routes/post_routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/users', userRoutes);
-//app.use ('/posts', postRoutes);
+app.use ('/posts', postRoutes);
 
 app.get('/', (req, res) => {
   res.send('API RESTful para usuarios y posts funcionando!');
